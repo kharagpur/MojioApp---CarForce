3130a25cadca98080f146917d9f414d23209b031
Kenneth Wong <kenneth.wong8(.a.t.)gmail.com>
2016-01-05 22:45:51 -0800

    BootStrap and CarForce Image

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,8 @@ module.exports = function(defaults) {
   });
 
   app.import('bower_components/mojio-js/dist/browser/MojioClient.js');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
